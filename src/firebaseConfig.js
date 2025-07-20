cf1643d22a2ccf06bb21984a2f9333bfe0bbefb4
Divyansh Gupta <104493538+divyansh980@users.noreply.github.com>
2025-07-20 14:28:47 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzbaZnMX2SRJo6RZtb_G-36Bt9hPzw5eM",
-  authDomain: "admindash-749f2.firebaseapp.com",
-  projectId: "admindash-749f2",
-  storageBucket: "admindash-749f2.firebasestorage.app",
-  messagingSenderId: "500779392983",
-  appId: "1:500779392983:web:356f2b48e7fe135f2cf592",
-  measurementId: "G-QCSWTKQ7FK"
+  apiKey: "API KEY ADD HERE",
+  authDomain: "admindash.firebaseapp.com",
+  projectId: "admindash",
+  storageBucket: "admindash.firebasestorage.app",
+  messagingSenderId: "",
+  appId: "1:500779392983:web:",
+  measurementId: "G-QCSWT7FK"
 };
 
 const app = initializeApp(firebaseConfig);
